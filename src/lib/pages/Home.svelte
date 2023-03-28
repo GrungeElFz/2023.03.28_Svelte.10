2f1455e47d2c642e4d422de3cfdd080adada1903
GrungeElFz <elf0114_@hotmail.com>
2023-03-28 14:55:08 -0500

    Modified:   Added a let variable `values`, assigned its value to an object (src/lib/pages/Home.svelte)             Added 3 sets of <label> and <input> for username, email, and password

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,2 +1,24 @@
 <script>
+	let values = {
+		username: '',
+		email: '',
+		password: ''
+	};
 </script>
+
+<form>
+	<label for="username">Username:</label>
+	<br />
+	<input id="username" name="username" type="text" bind:value={values.username} />
+	<br />
+
+	<label for="email">Email:</label>
+	<br />
+	<input id="email" name="email" type="email" bind:value={values.email} />
+	<br />
+
+	<label for="password">Password:</label>
+	<br />
+	<input id="password" name="password" type="password" bind:value={values.password} />
+	<br />
+</form>
