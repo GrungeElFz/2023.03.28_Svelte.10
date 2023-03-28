3aa1d342908fa64772a781d5e2200b1b4b882243
GrungeElFz <elf0114_@hotmail.com>
2023-03-28 14:39:38 -0500

    Initialized:    Cloned from `git@github.com:GrungeElFz/2023.03.27_Svelte.09.git` repository

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+	import { onMount } from 'svelte';
+	import Head from './lib/Head.svelte';
+	import Home from './lib/pages/Home.svelte';
+	import Settings from './lib/pages/Settings.svelte';
+
+	let page;
+
+	onMount(onRouteChange);
+
+	function onRouteChange() {
+		const path = window.location.hash.slice(1);
+
+		if (path === '/') {
+			page = 'home';
+		} else if (path === '/settings') {
+			page = 'settings';
+		} else {
+			window.location.hash = '/';
+		}
+	}
+</script>
+
+<svelte:window on:hashchange={onRouteChange} />
+<Head />
+
+<nav>
+	<a href="#/">Home</a>
+	<a href="#/settings">Settings</a>
+</nav>
+
+{#if page === 'home'}
+	<Home />
+{:else if page === 'settings'}
+	<Settings />
+{/if}
+
+<style>
+</style>
