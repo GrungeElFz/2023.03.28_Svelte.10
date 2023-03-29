8c0918b262b2c09d11c99928fdaa2d9e5e245178
GrungeElFz <elf0114_@hotmail.com>
2023-03-29 17:48:45 -0500

    Modified:   Imported { v4 as uuid } from 'uuid' (src/lib/Form/Field.svelte)             Exported a let variable `name`             Exported a let variable `type = 'text'` 	          Exported a let variable `label = undefined` 	          Exported a let variable `validate = undefined`             Defined a const variable `id = uuid()`
                Added a <div>
                - Added an if statement with the condition of `label`
                - Encapsulated <label for={id}> in the statement
                - Encapsulated {label} in the <label>
                - Added <input> with `placeholder={label}`

---
+++ src/lib/Form/Field.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { v4 as uuid } from 'uuid';
+
+	export let name;
+	export let type = 'text';
+	export let label = undefined;
+	export let validate = undefined;
+
+	const id = uuid();
+</script>
+
+<div>
+	{#if label}
+		<label for={id}>{label}</label>
+	{/if}
+	<input {id} {name} {type} placeholder={label} />
+</div>
